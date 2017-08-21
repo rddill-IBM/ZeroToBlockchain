d65c2d0fed2d39af68bea42424d5b911d1a9d69f
rddill-IBM <rddill@us.ibm.com>
2017-08-21 16:55:28 -0400

    windows install process and images created

---
+++ setup_Windows.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+ 
+ YELLOW='\033[1;33m'
+ RED='\033[1;31m'
+ GREEN='\033[1;32m'
+ RESET='\033[0m'
+
+# indent text on echo
+function indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";;
+    *)      sed -u "$c";;
+  esac
+}
+
+# Grab the current directory
+function getCurrent() 
+    {
+        showStep "getting current directory"
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        THIS_SCRIPT=`basename "$0"`
+        showStep "Running '${THIS_SCRIPT}'"
+    }
+
+# displays where we are, uses the indent function (above) to indent each line
+function showStep ()
+    {
+        echo -e "${YELLOW}=====================================================" | indent
+        echo -e "${RESET}-----> $*" | indent
+        echo -e "${YELLOW}=====================================================${RESET}" | indent
+    }
+
+
+# create a folder for the hyperledger fabric images and get them from the server
+function install_hlf ()
+    {
+        if [[ $HLF_INSTALL == "true" ]]; then
+            if [ ! -d "$HLF_INSTALL_PATH" ]; then
+                showStep "creating hlf tools folder $HLF_INSTALL_PATH "
+                mkdir -p "$HLF_INSTALL_PATH"
+            fi
+            cd "$HLF_INSTALL_PATH"
+            pwd
+            showStep "retrieving image scripts from git"
+            curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
+            showStep "unzipping images"
+            unzip -o fabric-dev-servers.zip
+            showStep "making scripts executable"
+            dos2unix `ls *.sh`
+            cd fabric-scripts/hlfv1
+            dos2unix `ls *.sh`
+            showStep "getting docker images for HyperLedger Fabric V1"
+            setx FABRIC_VERSION "hlfv1"
+            export FABRIC_VERSION=hlfv1
+            cd $HLF_INSTALL_PATH
+            ./downloadFabric.sh
+            showStep "installing platform specific binaries for OSX"
+            curl -sSL https://goo.gl/eYdRbX | bash
+            setx path "$HLF_INSTALL_PATH/bin";$PATH
+            setx HLF_INSTALL_PATH "$HLF_INSTALL_PATH"
+        else   
+            showStep "${RED}skipping HyperLedger Fabric install"
+        fi
+    }
+function printHelp ()
+{
+    printHeader
+    echo ""
+    echo -e "${RESET} options for this exec are: "
+    echo -e "${GREEN}-d ${RESET}defaults to ${GREEN}true${RESET}. use ${YELLOW}-d false ${RESET}if you do not want to have hypleledger node images verified" | indent
+    echo -e "${GREEN}-p ${RESET}defaults to ${GREEN}${HOME}/fabric-tools${RESET}. use ${YELLOW}-p ${HOME}/your/preferred/path/fabric-tools/your/path/here ${RESET}if you want to install hyperledger fabric tools elsewhere." | indent
+    echo -e "\t\tonly valid with -d true, ignored otherwise" | indent
+    echo ""
+    echo ""
+}
+
+# print the header information for execution
+function printHeader ()
+{
+    echo ""
+    echo -e "${YELLOW}installation script for the Zero To Blockchain Series" | indent
+    echo -e "${RED}This is for Windows BASH ONLY" | indent
+    echo -e "${YELLOW}The script will finish by downloading the docker images for hyperledger${RESET}" | indent
+    echo ""
+}
+# get the command line options
+
+HLF_INSTALL="true"
+HLF_INSTALL_PATH="${HOME}/fabric-tools"
+
+ while getopts "h:d:p:" opt; 
+do
+    case "$opt" in
+        h|\?)
+        printHelp
+        exit 0
+        ;;
+        d)  showStep "option passed for hyperledger docker install is: '$OPTARG'" 
+            if [[ $OPTARG != "" ]]; then 
+                HLF_INSTALL=$OPTARG 
+            fi
+        ;;
+        p)  showStep "option passed for fabric tools path is: '$OPTARG'" 
+            if [[ $OPTARG != "" ]]; then 
+                HLF_INSTALL_PATH=$OPTARG 
+            fi
+        ;;
+    esac
+ done
+
+    printHeader
+    echo  "Parameters:"
+    echo -e "Install HyperLedger Fabric? ${GREEN} $HLF_INSTALL ${RESET}" | indent
+    echo -e "Hyperledger fabric tools install path? ${GREEN} $HLF_INSTALL_PATH ${RESET}" | indent
+
+
+    getCurrent
+    showStep "installing hyperledger docker images"
+    install_hlf
+    showStep "installation complete"
